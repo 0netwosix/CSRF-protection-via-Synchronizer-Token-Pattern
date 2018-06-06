3c82f4104c2311c0c9b0818f2446cbe157773ab7
Nuwan Fernando <nuwannc2@hotmail.com>
2018-06-07 02:56:39 +0530

    Add files via upload

--- Solution_1/endpoint.php
+++ Solution_1/endpoint.php
@@ -1,7 +1,4 @@
 <?php
-	$session_id = $_POST['session_id'];
-
-	$current_token = sha1($session_id);
-
-	echo json_encode(array("id" => $current_token));
+	session_start();
+	echo json_encode(array("id" => $_SESSION['token']));
 ?>
